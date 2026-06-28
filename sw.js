a499bc1e1f4c6b6a5346e666fc3fb5a14e47dad6
schoolraaliberte-cpu <schoolraaliberte@gmail.com>
2026-06-28 07:13:34 +0000

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'gmb24-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/firebase.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
